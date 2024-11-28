d7a1d9991272c407c174de292f194e4d4ab6bdf8
Alexa Duarte <aduarte@konradgroup.com>
2024-11-28 11:12:53 -0600

    Fix test

--- api/junit5-migration-maven/src/test/java/com/example/project/SecondTest.java
+++ api/junit5-migration-maven/src/test/java/com/example/project/SecondTest.java
@@ -28,7 +28,7 @@ class SecondTest {
 		Allure.label("parentSuite", "API");
         Allure.suite("mySecondTest");
         Allure.label("subSuite", "mySecondTest");
-		assertEquals(1, 2, "2 is not equal to 1");
+		assertEquals(2, 2, "2 is not equal to 1");
 	}
 
 	@Test
