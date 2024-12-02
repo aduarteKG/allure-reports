22208c99c7b808830137ad5c32a7bf416c321f28
Alexa Duarte <aduarte@konradgroup.com>
2024-12-02 17:01:21 -0600

    Add cucumber project

---
+++ api/playwright-java-cucumber/src/test/java/hooks/Hooks.java
@@ -0,0 +1,45 @@
+package hooks;
+
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Tracing;
+
+import factory.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import utils.WebActions;
+
+public class Hooks {
+    public DriverFactory driverFactory;
+    public Page page;
+
+    @Before
+    public void launchBrowser() {
+         Allure.label("parentSuite", "Tests for web interface");
+
+        String browserName = WebActions.getProperty("browser");  //Fetching browser value from config file
+        driverFactory = new DriverFactory();
+        page = driverFactory.initDriver(browserName); // Passing browser name to launch the browser
+    }
+
+    //After runs in reverse order so order=1 will run first
+    @After(order = 0)
+    public void quitBrowser() {
+        page.close();
+    }
+
+    @After(order = 1)
+    public void takeScreenshotAndTrace(Scenario scenario) {
+        if (scenario.isFailed()) {
+            String screenshotName = scenario.getName().replaceAll("", "_"); //Replace all space in scenario name with underscore
+            byte[] sourcePath = page.screenshot();
+            scenario.attach(sourcePath, "image/png", screenshotName);  //Attach screenshot to report if scenario fails
+            DriverFactory.context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("target/" + screenshotName + ".zip")));
+        }
+    }
+
+
+}
